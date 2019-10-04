0d1d7f0196221df5e4d3572c0f26723e96b8cdbf
Nick Taras <nicktaras@hotmail.co.uk>
2019-10-04 23:50:53 +1000

    annotated current code

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var port = new SerialPort('/dev/tty.MindWaveMobile-SerialPo-15', {
   baudRate: 9600
 });
 
-port.write('main screen turn on', function (err) {
+// device turned on
+port.write('turn on', function (err) {
   if (err) {
     return console.log('Error on write: ', err.message);
   }
   console.log('message written');
 });
 
+// incoming data
 port.on('data', function (data) {
   console.log('Data:', data);
 });
